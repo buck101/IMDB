3c1220ada8ae7f8aeb0c2205f82e4b5a3e228aec
chenhui <chenhui101@gmail.com>
2015-02-12 00:04:42 +0800

    regular

--- web_spider.py
+++ web_spider.py
@@ -57,14 +57,17 @@ class Xun_Lei_Hao_Html_Parser(SGMLParser):
 
         #self.re_cn_name = re.compile(ur'◎译　　名　(.*)')
         #【中文名称】：
-        #self.re_cn_name = re.compile(ur'\u25ce\u8bd1\u3000\u3000\u540d\u3000(.*?)\u0020\u25ce')
-        #self.re_cn_name = re.compile(ur'\u3010\u4e2d\u6587\u540d\u79f0\u3011\uff1a(.*?)\u0020\u0020\u3010')
 
-        #s1 = ur'\u25ce\u8bd1\u3000\u3000\u540d\u3000(.*?)\u0020\u25ce'
-        #s2 = ur'\u3010\u4e2d\u6587\u540d\u79f0\u3011\uff1a(.*?)\u0020\u0020\u3010')
-        #self.re_cn_name = re.compile(ur'%s|%s' % (ur'\u25ce\u8bd1\u3000\u3000\u540d\u3000(.*?)\u0020\u25ce',  ur'\u3010\u4e2d\u6587\u540d\u79f0\u3011\uff1a(.*?)\u0020\u0020\u3010'))
+        #self.re_cn_name_1 = re.compile(ur'\u25ce\u8bd1\u3000\u3000\u540d\u3000(.*?)\u0020\u25ce')
+        self.re_cn_name_1 = re.compile(ur'\u540f')
 
-        RE = ur'\u25ce\u8bd1\u3000\u3000\u540d\u3000(.*?)\u0020\u25ce'
+		#◎中文片名　风雨双流星  (\u25ce\u4e2d\u6587\u7247\u540d\u3000\u98ce\u96e8\u53cc\u6d41\u661f  \u25ce)
+        self.re_cn_name_2 = re.compile(ur'\u25ce\u4e2d\u6587\u7247\u540d\u3000+(.+?)\u0020+\u25ce')
+        #self.re_cn_name_2 = re.compile(ur'\u302d')
+
+        #self.re_cn_name_3 = re.compile(ur'\u25ce\u4e2d\u6587\u7247\u540d\u3000(.*?)\u0020\u0020\u25ce')
+        self.re_cn_name_3 = re.compile(ur'\u5643')
+
 
 
         #self.re_en_name = re.compile(ur'◎片　　名　(.*)')
@@ -114,59 +117,71 @@ class Xun_Lei_Hao_Html_Parser(SGMLParser):
         if self.is_ul:
             self.is_ul = False
 
-    def end_div(self):
-        if self.is_article_content:
-            self.is_article_content = False
-            text = ' '.join(self.article_contents)
+	def end_div(self):
+		if self.is_article_content:
+			self.is_article_content = False
+			text = ' '.join(self.article_contents)
 
-            all_found = True;
+			all_found = True;
 
-            search = self.re_year.search(text)
-    	    if search:
-                self.movie.year = int(''.join(search.groups()))
-                print "year ", self.movie.year
-            else:
-                print "year not found"
-                all_found = False
+			search = self.re_cn_name_1.search(text)
+			if search:
+				self.movie.cn_name = ''.join(search.groups())
+				print "cname=", self.movie.cn_name
+			else:
+				search = self.re_cn_name_2.search(text)
+				if search:
+					self.movie.cn_name = ''.join(search.groups())
+					print "cname=", self.movie.cn_name
+				else:
+					search = self.re_cn_name_3.search(text)
+					if search:
+						self.movie.cn_name = ''.join(search.groups())
+						print "cname=", self.movie.cn_name
+					else:
+						print "cname not found"
+						all_found = False
+			#print repr(self.movie.cn_name)
+			time.sleep(10)
 
-            search = self.re_en_name.search(text)
-    	    if search:
-                self.movie.en_name = ''.join(search.groups())
-                print "ename ", self.movie.en_name
-            else:
-                print "ename not found"
-                all_found = False
+            #search = self.re_year.search(text)
+    	    #if search:
+            #    self.movie.year = int(''.join(search.groups()))
+            #    print "year ", self.movie.year
+            #else:
+            #    print "year not found"
+            #    all_found = False
 
-            search = self.re_cn_name.search(text)
-    	    if search:
-                self.movie.cn_name = ''.join(search.groups())
-                print "cname ", self.movie.cn_name
-            else:
-                print "cname not found"
-                all_found = False
+            #search = self.re_en_name.search(text)
+    	    #if search:
+            #    self.movie.en_name = ''.join(search.groups())
+            #    print "ename ", self.movie.en_name
+            #else:
+            #    print "ename not found"
+            #    all_found = False
 
-            search = self.re_category.search(text)
-    	    if search:
-                self.movie.category = ''.join(search.groups())
-                print "category ", self.movie.category
-            else:
-                print "category not found"
-                all_found = False
+            #search = self.re_category.search(text)
+    	    #if search:
+            #    self.movie.category = ''.join(search.groups())
+            #    print "category ", self.movie.category
+            #else:
+            #    print "category not found"
+            #    all_found = False
 
-            search = self.re_country.search(text)
-    	    if search:
-                self.movie.country = ''.join(search.groups())
-                print "country ", self.movie.country
-            else:
-                print "country not found"
-                all_found = False
+            #search = self.re_country.search(text)
+    	    #if search:
+            #    self.movie.country = ''.join(search.groups())
+            #    print "country ", self.movie.country
+            #else:
+            #    print "country not found"
+            #    all_found = False
 
-            if all_found == False:
-                print text
-                print repr(text)
-
-            self.article_contents = []
-            time.sleep(1)
+			if all_found == False:
+				print text
+				print repr(text)
+			
+			self.article_contents = []
+			time.sleep(1)
 
 
 
@@ -281,7 +296,7 @@ try:
     #print html
     
     web_spider = Xun_Lei_Hao_Spider("http://xunleihao.com/jingdiandianying/", "xunleihao/")
-    web_spider.page_dl_list(1, 163)
+    web_spider.page_dl_list(1, 1)
     web_spider.page_dl_movies()
 
 
