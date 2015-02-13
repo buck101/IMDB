9ccc2ccd2e921c40618ef8c9357de1e5bb33dd96
buck101 <chenhui101@gmail.com>
2015-02-13 11:51:09 +0800

    regular

--- web_spider.py
+++ web_spider.py
@@ -23,6 +23,19 @@ headers = {
     'Referer':my_url
 }
 
+C2D = {
+        'JDDY':'xunleihao/JDDY/',
+        'OMDY':'xunleihao/OMDY/',
+        'RHDY':'xunleihao/RHDY/',
+        'GTDY':'xunleihao/GTDY/',
+        'DLDY':'xunleihao/DLDY/',
+        'OMJ':'xunleihao/OMJ/',
+        'RHJ':'xunleihao/RHJ/',
+        'DLJ':'xunleihao/DLJ/',
+        'ZYP':'xunleihao/ZYP/',
+        'DM':'xunleihao/DM/',
+}
+
 class movie:
     def __init__(self):
         self.imdb_id = ''
@@ -50,71 +63,23 @@ class Xun_Lei_Hao_Html_Parser(SGMLParser):
 
         self.movies = {}
         self.id = ''
+        self.category = ''
         self.movie = movie()
         self.article_contents = []
 
         self.re_movie = re.compile(r'\w+\/(\w+\.html)')
 
-        #self.found_count = 0
-        #self.not_found_count = 0
-
-
-        #self.re_cn_name = []
-        ##【中文片名】父辈的旗帜/硫磺岛浴血战/硫磺岛的英雄们/战火旗迹  
-        #self.re_cn_name.append(re.compile(ur'\u3010\u4e2d\u6587\u7247\u540d\u3011.(.+?)\u3010'))
-
-        ##◎中文译名 彗星美人  
-        #self.re_cn_name.append(re.compile(ur'\u25ce\u4e2d\u6587\u8bd1\u540d.(.+?)\u25ce'))
-	##◎中文片名　风雨双流星  (\u25ce\u4e2d\u6587\u7247\u540d\u3000\u98ce\u96e8\u53cc\u6d41\u661f  )
-        #self.re_cn_name.append(re.compile(ur'\u25ce\u4e2d\u6587\u7247\u540d.(.+?)\u25ce'))
-        ##中文名称：光荣岁月 
-        #self.re_cn_name.append(re.compile(ur'\u4e2d\u6587\u540d\u79f0.(.+?)\u0020+'))
-
-        ##◎中 文 名: 
-        #self.re_cn_name.append(re.compile(ur'\u25ce\u4e2d.*?\u6587.*?\u540d.(.+?)\u25ce'))
-        ##中 文 名: 败家仔  
-        #self.re_cn_name.append(re.compile(ur'\u4e2d.*\u6587.*\u540d.(.+?)\u0020+'))
-
-        ##【译　　名】　小鬼大间谍/非常小特务/神童特工/特工神童  【 ()
-        #self.re_cn_name.append(re.compile(ur'\u3010\u8bd1.*?\u540d\u3011(.+?)\u3010'))
-        ##◎译　　名  拨云见日 ◎ (\u25ce\u8bd1\u3000\u3000\u540d  \u62e8\u4e91\u89c1\u65e5 )
-        #self.re_cn_name.append(re.compile(ur'\u25ce\u8bd1.*?\u540d.(.+?)\u25ce'))
-        ##译　　名：爱再来一次  
-        #self.re_cn_name.append(re.compile(ur'\u8bd1.*?\u540d.(.+?)\u0020+'))
-
-
-        ##片名：爱的逃兵  　
-        #self.re_cn_name.append(re.compile(ur'\u7247\u540d.(.+?)\u0020+'))
-        ##别名：挥洒烈爱/笔姬别恋   
-        #self.re_cn_name.append(re.compile(ur'\u522b\u540d.(.+?)\u0020+'))
-        ##◎片　　名 迷途英雄  
-        #self.re_cn_name.append(re.compile(ur'\u25ce\u7247\u3000*\u540d(.+?)\u0020+'))
-        ##【别　　名】蝙蝠侠：开战时刻/蝙蝠侠前传/蝙蝠侠5：侠影之谜
-        #self.re_cn_name.append(re.compile(ur'\u3010\u522b.*\u540d\u3011(.+?)\u0020+'))
-        ##【片 　　名】冒险王  
-        #self.re_cn_name.append(re.compile(ur'\u3010\u7247.*\u540d\u3011(.+?)\u0020+'))
-
-        ##【外文译名】铁道员/鉄道員  
-        #self.re_cn_name.append(re.compile(ur'\u3010\u5916\u6587\u8bd1\u540d\u3011(.+?)\u0020+'))
-
-
-        ##self.re_en_name = re.compile(ur'◎片　　名　(.*)')
-        #self.re_en_name = re.compile(ur'\u25ce\u7247\u3000\u3000\u540d\u3000(.*?)\u0020\u25ce')
-
-        ##self.re_year = re.compile(ur'◎年　　代　(\d+)')
-        #self.re_year = re.compile(ur'\u25ce\u5e74\u3000\u3000\u4ee3\u3000(.*?)\u0020\u25ce')
-
-        ##self.re_category = re.compile(ur'◎类　　别　(.*)')
-        #self.re_category = re.compile(ur'\u25ce\u7c7b\u3000\u3000\u522b\u3000(.*?)\u0020\u25ce')
-
-        ##self.re_country = re.compile(ur'◎国　　家　(.*)')
-        #self.re_country = re.compile(ur'\u25ce\u56fd\u3000\u3000\u5bb6\u3000(.*?)\u0020\u25ce')
 
     def start_ul(self, attrs):
         for attr in attrs:
             if attr[1] == 'llst06':
                 self.is_ul = True
 
+    def end_ul(self):
+        if self.is_ul:
+            self.is_ul = False
+
+    # for dl_list
     def start_a(self, attrs):
         if self.is_ul:
     	    href = [v for k, v in attrs if k == 'href']
@@ -123,10 +88,25 @@ class Xun_Lei_Hao_Html_Parser(SGMLParser):
                 self.is_movie = True
                 self.id = ''.join(search.groups()) 
 
+    def end_a(self):
+        if self.is_movie:
+            self.is_movie = False
+            self.movie.category = self.category
+            self.movies[self.id] = copy.copy(self.movie)
+
+
+    # for dl_movie
     def start_div(self, attrs):
         for attr in attrs:
             if attr[1] == 'articlecontent':
                 self.is_article_content = True
+
+    def end_div(self):
+    	if self.is_article_content:
+    	    self.is_article_content = False
+    	    text = ' '.join(self.article_contents).replace('\r\n', '')
+
+            #self.movie.__init__()
 
     def handle_data(self, text):
         if self.is_ul and self.is_movie:
@@ -136,120 +116,37 @@ class Xun_Lei_Hao_Html_Parser(SGMLParser):
             self.article_contents.append(text.decode('GBK', 'ignore'))
 
 
-    def end_a(self):
-        if self.is_movie:
-            self.is_movie = False
-            self.movies[self.id] = copy.copy(self.movie)
-
-    def end_ul(self):
-        if self.is_ul:
-            self.is_ul = False
-
-    def end_div(self):
-    	if self.is_article_content:
-    	    self.is_article_content = False
-    	    text = ' '.join(self.article_contents).replace('\r\n', '')
-
-    	    #found = False;
-
-            #for p in self.re_cn_name:
-            #    search = p.search(text)
-    	    #    if search:
-    	    #        self.movie.cn_name = ''.join(search.groups())
-            #        found = True
-            #        break
-    	    #if found:
-    	    #	print "cname=", self.movie.cn_name.encode('UTF-8')
-            #    self.found_count = self.found_count + 1 
-    	    #else:
-            #    self.not_found_count = self.not_found_count + 1
-    	    #	print "cname not found"
-            #    print text.encode('UTF-8')
-            #    print repr(text)
-
-
-            #search = self.re_year.search(text)
-    	    #if search:
-            #    self.movie.year = int(''.join(search.groups()))
-            #    print "year ", self.movie.year
-            #else:
-            #    print "year not found"
-            #    all_found = False
-
-            #search = self.re_en_name.search(text)
-    	    #if search:
-            #    self.movie.en_name = ''.join(search.groups())
-            #    print "ename ", self.movie.en_name
-            #else:
-            #    print "ename not found"
-            #    all_found = False
-
-            #search = self.re_category.search(text)
-    	    #if search:
-            #    self.movie.category = ''.join(search.groups())
-            #    print "category ", self.movie.category
-            #else:
-            #    print "category not found"
-            #    all_found = False
-
-            #search = self.re_country.search(text)
-    	    #if search:
-            #    self.movie.country = ''.join(search.groups())
-            #    print "country ", self.movie.country
-            #else:
-            #    print "country not found"
-            #    all_found = False
-
-	    #self.article_contents = []
-	    #time.sleep(1)
-
-
-
-            #if self.movie.en_name == '':
-            #    print "en_name not found"
-            #    time.sleep(1)
-
-            #if self.movie.cn_name == '':
-            #    print "cn_name not found"
-            #    time.sleep(1)
-
-            #if self.movie.country == '':
-            #    print "country not found"
-            #    time.sleep(1)
-
-            #if self.movie.category == '':
-            #    print "category not found"
-            #    time.sleep(1)
-
-            #if self.movie.year == 0:
-            #    print "year not found"
-            #    time.sleep(1)
-
-            #self.movie.__init__()
 
 class Xun_Lei_Hao_Spider:
 
-    def __init__(self, url, dir_name):
-    	self.url = url
-    	self.dir_name = dir_name
+    def __init__(self):
         self.html_parser = Xun_Lei_Hao_Html_Parser()
     
-    def page_dl_list(self, begin_page, end_page):
-    	for i in range(begin_page, end_page + 1):
+    def page_dl_list(self, url, category):
+    	self.url = url
+    	self.dir_name = C2D[category]
+        self.html_parser.category = category
+
+        i = 1
+        while (True):
     	    remote_file_name = "list_5_" + str(i) + ".html"
     	    local_file_name = "list_5_" + string.zfill(i, 3) + ".html"
             if os.path.isfile(self.dir_name + local_file_name):
     	        f = open(self.dir_name + local_file_name, 'r+')
-    	        #print "R[" + self.url + remote_file_name + "]"
+    	        print "R[" + self.url + remote_file_name + "]"
     	        page_content = ''.join(f.readlines())
     	        f.close()
             else:
     	        page_content = urllib2.urlopen(self.url + remote_file_name).read()
+                if page_content.decode('GBK', 'ignore').find(ur'\u60a8\u8bbf\u95ee\u7684\u5730\u5740\u4e0d\u5b58\u5728') != -1:
+                    print self.url + remote_file_name, "not found 404"
+                    break
     	        f = open(self.dir_name + local_file_name, 'w+')
-    	        #print "W[" + self.url + remote_file_name + "]"
+    	        print "W[" + self.url + remote_file_name + "]"
     	        f.write(page_content)
     	        f.close()
             self.html_parser.feed(page_content)
+            i = i + 1
 
     def page_dl_movies(self):
         print "Total:", len(self.html_parser.movies)
@@ -257,22 +154,18 @@ class Xun_Lei_Hao_Spider:
     	for (k, v) in self.html_parser.movies.items():
     	    remote_file_name = k
             local_file_name = k
-            if os.path.isfile(self.dir_name + "/movies/" + local_file_name):
-    	        f = open(self.dir_name + "/movies/" + local_file_name, 'r+')
+            if os.path.isfile("xunleihao/movies/" + local_file_name):
+    	        f = open("xunleihao/movies/" + local_file_name, 'r+')
     	        print "R>>>>" + self.url + remote_file_name
     	        page_content = ''.join(f.readlines())
     	        f.close()
             else:
     	        page_content = urllib2.urlopen(self.url + remote_file_name).read()
-    	        f = open(self.dir_name + "/movies/" + local_file_name, 'w+')
+    	        f = open("xunleihao/movies/" + local_file_name, 'w+')
     	        print "D>>>>" + self.url + remote_file_name
     	        f.write(page_content)
     	        f.close()
             self.html_parser.feed(page_content)
-
-
-
-
 
 
 
@@ -315,13 +208,19 @@ try:
     #html = res.read()
     #print html
     
-    web_spider = Xun_Lei_Hao_Spider("http://xunleihao.com/jingdiandianying/", "xunleihao/")
-    web_spider.page_dl_list(1, 163)
+    web_spider = Xun_Lei_Hao_Spider()
+    web_spider.page_dl_list('http://xunleihao.com/jingdiandianying/', 'JDDY')
+    web_spider.page_dl_list('http://xunleihao.com/oumeidianying/', 'OMDY')
+    web_spider.page_dl_list('http://xunleihao.com/rihandianying/', 'RHDY')
+    web_spider.page_dl_list('http://xunleihao.com/gangtaidianying/', 'GTDY')
+    web_spider.page_dl_list('http://xunleihao.com/daludianying/', 'DLDY')
+    web_spider.page_dl_list('http://xunleihao.com/oumeiju/', 'OMJ')
+    web_spider.page_dl_list('http://xunleihao.com/rihanju/', 'RHJ')
+    web_spider.page_dl_list('http://xunleihao.com/gangtaiju/', 'GTJ')
+    web_spider.page_dl_list('http://xunleihao.com/daluju/', 'DLJ')
+    web_spider.page_dl_list('http://xunleihao.com/zongyipian/', 'ZYP')
+    web_spider.page_dl_list('http://xunleihao.com/dongman/', 'DM')
     web_spider.page_dl_movies()
-
-    #print web_spider.html_parser.found_count
-    #print web_spider.html_parser.not_found_count
-
 
 
 except urllib2.URLError, e:
@@ -351,10 +250,9 @@ cursor.execute("set autocommit=1")
 
 try:
     cursor.execute("use movies")
-    cursor.execute("truncate table xlh_fileid2show_name")
+    cursor.execute("truncate table xunleihao")
     for (k, v) in web_spider.html_parser.movies.items():
-        #print "insert into xlh_fileid2show_name values('%s','%s')"  % (k, v.show_name.encode('UTF-8'));
-        cursor.execute("insert into xlh_fileid2show_name values('%s','%s')"  % (k, v.show_name.encode('UTF-8')));
+        cursor.execute("insert into xunleihao values('%s','%s','%s')"  % (k, v.show_name.encode('UTF-8'), v.category));
         conn.commit()
 
 except MySQLdb.Error, e:
